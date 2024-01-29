a787921dd897d2e1f74508e14f1cf86db65718e7
JesusGuti <jesusgonzales0968@gmail.com>
2024-01-29 15:39:55 -0400

    Fetch Project

---
+++ FreeCodeCampNews/script.js
@@ -0,0 +1,66 @@
+const authorContainer = document.getElementById('author-container');
+const loadMoreBtn = document.getElementById('load-more-btn');
+
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
+/**
+ * La API Fetch es una interfaz de JS para hacer request
+ * al servidor, el metodo fetch() se puede usar para hacer 
+ * GET, POST, PUT y PATCH
+**/
+
+/**
+ * El metodo fetch() regresa una Promise (placeholder de objeto)
+ * que será completado o rechazado dependiendo del exito del request
+ * Si una promesa es cumplida se resuelve a un objeto Response y se puede 
+ * acceder a Response con .then()
+**/
+
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
+  .then((res) => res.json())
+  .then((data) => {
+    authorDataArr = data;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));  
+  })
+  .catch((err) => {
+   authorContainer.innerHTML = '<p class="error-msg">There was an error loading the authors</p>';
+  });
+
+/**
+ * .catch() es un metodo asincrono que permite manejar errores
+ * Es util en caso de que la Promise es rechazada
+**/
+
+/**Con esto estamos dibujando más autores **/
+const fetchMoreAuthors = () => {
+  startingIndex += 8;
+  endingIndex += 8;
+
+  displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+  if (authorDataArr.length <= endingIndex) {
+    loadMoreBtn.disabled = true;
+  loadMoreBtn.style.cursor = "not-allowed";
+    loadMoreBtn.textContent = 'No more data to load';
+  }
+};
+
+const displayAuthors = (authors) => {
+  authors.forEach(({ author, image, url, bio }, index) => {
+    authorContainer.innerHTML += `
+    <div id="${index}" class="user-card">
+      <h2 class="author-name">${author}</h2>
+      <img class="user-img" src="${image}" alt="${author} avatar">
+      <div class="purple-divider"></div>
+      <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + '...' : bio}</p>
+      <a class="author-link" href="${url}" target="_blank">${author} author page</a>
+    </div>
+  `;
+  });
+};
+
+loadMoreBtn.addEventListener('click', fetchMoreAuthors);
+
+
+
